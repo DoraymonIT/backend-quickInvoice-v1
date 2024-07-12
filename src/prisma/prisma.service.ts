8e8eebecce307805a637a72a8743b7cf10ca62e6
AYOUB BENDRIMOU <53580861+DoraymonIT@users.noreply.github.com>
2024-07-12 11:35:33 +0100

    see now v1

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -9,7 +9,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
 
   // async enableShutdownHooks(app: INestApplication) {
   //   this.$on('beforeExit', async () => {
-  //     await app.close();
+  //     await app.close(); 
   //   });
   // }
   // After adding new columns , do this : npx prisma migrate dev anf then npx prisma studio 
