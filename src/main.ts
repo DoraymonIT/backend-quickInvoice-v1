32464150d420a9eb6a9f0238e2cb8e1be089d619
AYOUB BENDRIMOU <53580861+DoraymonIT@users.noreply.github.com>
2024-07-20 12:06:26 +0100

    test the cors issu 4e

--- src/main.ts
+++ src/main.ts
@@ -24,12 +24,12 @@ async function bootstrap() {
 
 
 
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-      next();
-  });
+  //   app.use((req, res, next) => {
+  //     res.header('Access-Control-Allow-Origin', '*');
+  //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //     next();
+  // });
 //   app.enableCors({
 //     origin: true,
 //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
